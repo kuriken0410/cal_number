e61a7a2556dd3d95c6dac448cea1946651636fa0
kuriken0410 <67869934+kuriken0410@users.noreply.github.com>
2020-11-15 00:52:08 +0900

    [add]controller-view
    localhost:8000/calculateにアクセスしたらnum.blade.phpというビューファイルを表示させる

--- app/Http/Controllers/CalculateController.php
+++ app/Http/Controllers/CalculateController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class CalculateController extends Controller
 {
-    //
+  public function getCalc(Request $request){
+    return view('calculate.num');
+　　//calculateディレクトリのnum.blade.phpを表示
+  }
 }
