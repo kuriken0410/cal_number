c9f7c639ad4fe7cd34604114d94b92acbabc0f25
kuriken0410 <67869934+kuriken0410@users.noreply.github.com>
2020-11-15 01:03:45 +0900

    [add]controller

--- app/Http/Controllers/CalculateController.php
+++ app/Http/Controllers/CalculateController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 class CalculateController extends Controller
 {
   public function getCalc(Request $request){
+    
+    $data['exchange'] = $request->input('usdoller');
+    $data['jpyen'] = ($data['exchange'] * 110);
+
     return view('calculate.num');
   }
 }
+
+//calculateディレクトリのnum.blade.phpを表示する
