4f05890638a4e5cd06261862cc7943d9377f6d5e
kuriken0410 <67869934+kuriken0410@users.noreply.github.com>
2020-11-21 23:05:25 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('num');
 });
 
 Route::get('/calculate', 'App\Http\Controllers\CalculateController@getcalc');
