5ce8cceaa384ab5bddbd65b4082ec7500464843f
kuriken0410 <67869934+kuriken0410@users.noreply.github.com>
2020-11-22 01:20:47 +0900

    [fix]route

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/calculate', 'App\Http\Controllers\CalculateController@getcalc');
+
+Route::get('/', 'App\Http\Controllers\CalculateController@getcalc');
